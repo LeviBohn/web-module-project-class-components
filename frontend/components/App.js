30e5c001d1bc806b4bbe72768a3dbb34d4df9474
Levi Bohn <levibohn@gmail.com>
2023-06-01 07:58:57 -0500

    completed project alongside solution video.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -20,16 +20,56 @@ export default class App extends React.Component {
       ]
     }
   }
+
+  handleAdd = (name) => {
+
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false
+    };
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+  }
+
+  handleToggle = (clickedId) => {
+
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(todo => {
+        if (todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+
+        return todo;
+      })
+    })
+  }
+
   render() {
     const { todos } = this.state;
-    console.log(todos);
+
     return (
       <div>
         <h1>Todos</h1>
-
-        < TodoList todos={todos}/>
-        < Form />
-        <button>clear</button>
+        < TodoList handleToggle={this.handleToggle} todos={todos} />
+        < Form handleAdd={this.handleAdd} />
+        <button onClick={this.handleClear}>clear</button>
 
       </div>
     )
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.props);
+    this.props.handleAdd(this.state.input);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      input: e.target.value
+    });
+  }
+
   render() {
     return (
       <form>
-        <input />
-        <button>add</button>
+        <input onChange={this.handleChange} />
+        <button onClick={this.handleSubmit}>add</button>
       </form>
     );
   }
